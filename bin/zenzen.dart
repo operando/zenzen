a63601a3f17b877d8ad9bc538616d4109207a3ff
Shinobu Okano <okanoshinobu8@gmail.com>
2020-10-25 02:11:15 +0900

    Add config option

--- bin/zenzen.dart
+++ bin/zenzen.dart
@@ -11,7 +11,24 @@ import 'package:zenzen/command/version_command.dart';
 
 Future<void> main(List<String> arguments) async {
   FilesystemConfigLoader.use();
-  var cfg = await loadConfig();
+
+  final cr = CommandRunner('zenzen',
+      'leave from many channels on Slack.You can also rejoin the channel from which you left.');
+  final version = VersionCommand();
+  cr.addCommand(version);
+
+  final parser = cr.argParser
+    ..addOption('config', abbr: 'c', help: 'config file path')
+    ..addFlag('version', abbr: 'v', negatable: false, help: version.description,
+        callback: (f) {
+      if (f) {
+        cr.run(['version']);
+        exit(0);
+      }
+    });
+
+  final result = parser.parse(arguments);
+  final cfg = await loadConfig(result['config'] ?? 'config.toml');
 
   final token = cfg['slack']['token'];
   if (token == null) {
@@ -29,19 +46,8 @@ Future<void> main(List<String> arguments) async {
     print('${record.level.name}: ${record.time}: ${record.message}');
   });
 
-  final version = VersionCommand();
-  final cr = CommandRunner('zenzen',
-      'leave from many channels on Slack.You can also rejoin the channel from which you left.');
   final notLeaveChannels = cfg['slack']['not_leave_channels'];
   cr
-    ..argParser.addFlag('version',
-        abbr: 'v', negatable: false, help: version.description, callback: (f) {
-      if (f) {
-        cr.run(['version']);
-        exit(0);
-      }
-    })
-    ..addCommand(version)
     ..addCommand(JoinCommand(token, cfg['slack']['rejoin_channel_json_file']))
     ..addCommand(LeaveCommand(token, cfg['slack']['user_id'],
         notLeaveChannels ?? UnmodifiableListView(List.empty())))
